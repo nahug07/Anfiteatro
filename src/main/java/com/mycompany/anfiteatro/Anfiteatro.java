acfe5bdfe6e63ccaabdec9f84a8f3877f5c72adc
nahuelgrijalba <nahug07@gmail.com>
2024-01-03 11:08:56 -0300

    first commit

---
+++ src/main/java/com/mycompany/anfiteatro/Anfiteatro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.anfiteatro;
+
+/**
+ *
+ * @author nahuelgrijalba
+ */
+public class Anfiteatro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
