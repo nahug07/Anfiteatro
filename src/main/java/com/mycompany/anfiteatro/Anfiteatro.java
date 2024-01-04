0851fc0c556fdfdb983ff535f9f17e40741afb1c
nahuelgrijalba <nahug07@gmail.com>
2024-01-04 07:19:52 -0300

    feat: add reservation system

--- src/main/java/com/mycompany/anfiteatro/Anfiteatro.java
+++ src/main/java/com/mycompany/anfiteatro/Anfiteatro.java
@@ -1,16 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.anfiteatro;
+import java.util.Scanner;
 
-/**
- *
- * @author nahuelgrijalba
- */
 public class Anfiteatro {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        
+        char asientos[][] = new char [10][10];
+        boolean bandera = false;
+        Scanner teclado = new Scanner(System.in);
+        int fila, asiento;
+        String salida;
+        
+        for(int f=0; f<10; f++){
+            for(int c=0; c<10; c++){
+                asientos[f][c] = 'L';
+            } 
+        }
+        
+        System.out.println("------ BIENVENIDO AL SISTEMA DE RESERVAS ------");  
+        
+        while(bandera != true){
+            
+            System.out.println("Ingrese Fila y Asiento a reservar");
+            System.out.print("Fila (entre 0 y 9)");
+            fila = teclado.nextInt();
+            
+            System.out.print("Asiento (entre 0 y 9)");
+            asiento = teclado.nextInt();
+            
+            if(asientos[fila][asiento]=='L') {
+                asientos[fila][asiento] = 'X';
+                System.out.println("El asiento fue reservado correctamente");
+            } else {
+                System.out.println("El asiento esta ocupado");
+            }
+            
+            System.out.println("¿Desea finalizar la reserva? S: Si, Cualquier tecla: No");
+            salida = teclado.next();
+            
+            if(salida.equalsIgnoreCase("S")){
+                bandera = true;
+            }
+        }
+        
+    }                    
 }
