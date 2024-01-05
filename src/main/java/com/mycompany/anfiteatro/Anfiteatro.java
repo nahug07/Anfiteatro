677c7ed35a2efb7b1a70e4da2d3b1f134598c103
nahuelgrijalba <nahug07@gmail.com>
2024-01-05 07:57:39 -0300

    fix: show seat map

--- src/main/java/com/mycompany/anfiteatro/Anfiteatro.java
+++ src/main/java/com/mycompany/anfiteatro/Anfiteatro.java
@@ -5,16 +5,15 @@ public class Anfiteatro {
 
     public static void main(String[] args) {
         
-        char asientos[][] = new char [10][10];
+        char asientos[][] = new char [11][11];
         boolean bandera = false;
         Scanner teclado = new Scanner(System.in);
         int fila = 0, asiento = 0;
         String salida;
         String mostrarMapa;
-        boolean error = true;
         
-        for(int f=0; f<10; f++){
-            for(int c=0; c<10; c++){
+        for(int f=1; f<11; f++){
+            for(int c=1; c<11; c++){
                 asientos[f][c] = 'L';
             } 
         }
@@ -30,16 +29,17 @@ public class Anfiteatro {
                 dibujarMapa(asientos);
             }
             
+            boolean error = true;
             while(error == true){
                 System.out.println("\n Ingrese Fila y Asiento a reservar");
-                System.out.print("Fila (entre 0 y 9)");
+                System.out.print("Fila (entre 1 y 10)");
                 fila = teclado.nextInt();
             
-                System.out.print("Asiento (entre 0 y 9)");
+                System.out.print("Asiento (entre 1 y 10)");
                 asiento = teclado.nextInt();
                 
-                if(fila <= 9 && fila >= 0) {
-                    if(asiento <= 9 && asiento >= 0) {
+                if(fila <= 10 && fila >= 1) {
+                    if(asiento <= 10 && asiento >= 1) {
                         error = false;
                     } else {
                         System.out.println("El numero de asiento no es valido");
@@ -53,10 +53,10 @@ public class Anfiteatro {
                 asientos[fila][asiento] = 'X';
                 System.out.println("El asiento fue reservado correctamente");
             } else {
-                System.out.println("El asiento esta ocupado");
+                System.out.println("El asiento esta ocupado, por favor elija otro");
             }
             
-            System.out.println("¿Desea finalizar la reserva? S: Si, Cualquier tecla: No");
+            System.out.println("\n ¿Desea finalizar la reserva? S: Si, Cualquier tecla: No");
             salida = teclado.next();
             
             if(salida.equalsIgnoreCase("S")){
@@ -67,9 +67,15 @@ public class Anfiteatro {
     }
     
     static void dibujarMapa(char asientos[][]){
-        for(int f=0; f<10; f++){
-            for(int c=0; c<10; c++){
-                System.out.print("[" + asientos[f][c] + "]");
+        for(int f=0; f<11; f++){
+            System.out.print((f != 10 ? "0" : "") + f + " "); 
+            
+            for(int c=1; c<11; c++){
+                if(f == 0) {
+                    System.out.print((c != 10 ? "0" : "") + c + " "); 
+                } else {
+                    System.out.print("[" + asientos[f][c] + "]");
+                }
             } 
             System.out.println("");    
         }
